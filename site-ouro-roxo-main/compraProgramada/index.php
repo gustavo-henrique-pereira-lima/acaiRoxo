f57cf8d44239b4a99f6cd72d73279276c785cc55
gustavo-henrique-pereira-lima <119140207+gustavo-henrique-pereira-lima@users.noreply.github.com>
2023-05-02 22:50:34 -0300

    Delete index.php

--- site-ouro-roxo-main/compraProgramada/index.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-    #UPDATE pedido SET Cliente = 'idcliente' WHERE = 'idPedido';
-    #UPDATE pedido SET Entregador = 'identregador' WHERE = 'idPedido';
-    #UPDATE pedido SET Produto = 'idproduto' WHERE = 'idPedido';
-
-    #gerar numero aleatorio "select idEntregador from entregador order by rand() limit 1;"
-    
-?>
